1e4ff2468762271a4b8c760f35e104e943e48cf0
jaykanjariya <jaykanjariya28@gmail.com>
2019-04-03 00:12:03 +0530

    create templates and other files

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
+from . import forms
 
 # Create your views here.
+class SignUp(CreateView):
+    from_class = forms.UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
