377dbc6e6e70c319934d01211806c9fa81e5b768
jaykanjariya <jaykanjariya28@gmail.com>
2019-04-03 14:29:26 +0530

    complete account app

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib import auth
-
+from django.utils import timezone
 
 # Create your models here.
 class User(auth.models.User, auth.models.PermissionsMixin):
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name="accounts/login.html"),name='login'),
-    path('logout/', LogoutView.as_view(), name="logout"),
+    path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('signup/', views.SignUp.as_view(), name="signup"),
 ]
